d7e7c2e8cb2de093d74c16181193af172b65908c
AkromSaidxonov <101202313+AkromSaidxonov@users.noreply.github.com>
2022-11-30 10:24:29 +0500

    ss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { useTelegram } from "./components/hooks/useTelegram";
+import Header from "./components/header/Header";
 
 function App() {
-const {tg, onToggleButton} = useTelegram()
+  const { tg, onToggleButton } = useTelegram();
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
     <div>
-<button onClick={onToggleButton}>toggle</button>
+      <Header/>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
